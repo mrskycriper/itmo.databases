f54df4da7cc9b2e71cf87458a786708bdd88bda2
mrskycriper <32013287+mrskycriper@users.noreply.github.com>
2020-05-07 00:15:20 +0500

    Update lab01.sql

--- lab01/lab01.sql
+++ lab01/lab01.sql
@@ -1,68 +1,68 @@
--- task01 --
+--- task 1 ---
 select Name, Color, Size
 from Production.Product
 
--- task02 --
+--- task 2 ---
 select Name, Color, Size
 from Production.Product
 where ListPrice > 100
 
--- task03 --
+--- task 3 ---
 select Name, Color, Size
 from Production.Product
 where ListPrice < 100 and Color = 'Black'
 
--- task04 --
+--- task 4 ---
 select Name, Color, Size
 from Production.Product
 where ListPrice < 100 and Color = 'Black'
 order by ListPrice
 
--- task05 --
+--- task 5 ---
 select top 3 Name, Size
 from Production.Product
 where Color = 'Black'
 order by ListPrice desc
 
--- task06 --
+--- task 6 ---
 select Name, Color
 from Production.Product
 where Color is not null and Size is not null
 
--- task07 --
+--- task 7 ---
 select distinct Color
 from Production.Product
 where ListPrice between 10 and 50
 
--- task08 --
+--- task 8 ---
 select Color
 from Production.Product
 where Name like 'L_N%'
 
--- task09 --
+--- task 9 ---
 select Name
 from Production.Product
 where Name like '[M,D]__%'
 
--- task10 --
+--- task 10 ---
 select Name
 from Production.Product
 where datepart(year, SellStartDate) <= 2012
 
--- task11 --
+--- task 11 ---
 select Name
 from Production.ProductSubcategory
 
--- task12 --
+--- task 12 ---
 select Name
 from Production.ProductCategory
 
--- task13 --
+--- task 13 ---
 select FirstName
 from Person.Person
 where Title like 'Mr.'
 
--- task14 --
+--- task 14 ---
 select FirstName
 from Person.Person
 where Title is null
